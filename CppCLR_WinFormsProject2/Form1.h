5345ec73b8d976a7a5946c0845551de1842008b5
ml.salto@alumnos.upm.es <ml.salto@alumnos.upm.es>
2022-09-29 19:28:39 +0200

    pruebas

--- CppCLR_WinFormsProject2/Form1.h
+++ CppCLR_WinFormsProject2/Form1.h
@@ -34,11 +34,29 @@ namespace CppCLRWinFormsProject {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^ label1;
+
 	protected:
 	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::CheckBox^ checkBox1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::TextBox^ textBox5;
+	private: System::Windows::Forms::TextBox^ textBox6;
+	private: System::Windows::Forms::TextBox^ textBox7;
+	private: System::Windows::Forms::TextBox^ textBox8;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::Label^ label7;
+	private: System::Windows::Forms::Label^ label8;
+	private: System::Windows::Forms::Label^ label9;
+	private: System::Windows::Forms::Label^ label10;
+	private: System::Windows::Forms::TrackBar^ trackBar1;
+
+
 
 
 	private:
@@ -54,20 +72,27 @@ namespace CppCLRWinFormsProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox7 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox8 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->label8 = (gcnew System::Windows::Forms::Label());
+			this->label9 = (gcnew System::Windows::Forms::Label());
+			this->label10 = (gcnew System::Windows::Forms::Label());
+			this->trackBar1 = (gcnew System::Windows::Forms::TrackBar());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBar1))->BeginInit();
 			this->SuspendLayout();
-			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(88, 102);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(112, 13);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"me muero jhajajjajajaja";
 			// 
 			// label2
 			// 
@@ -77,38 +102,181 @@ namespace CppCLRWinFormsProject {
 			this->label2->Size = System::Drawing::Size(0, 13);
 			this->label2->TabIndex = 1;
 			// 
-			// button1
+			// textBox1
 			// 
-			this->button1->Location = System::Drawing::Point(100, 171);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
-			this->button1->TabIndex = 2;
-			this->button1->Text = L"destruccion";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			this->textBox1->Location = System::Drawing::Point(58, 72);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(43, 20);
+			this->textBox1->TabIndex = 4;
 			// 
-			// checkBox1
+			// label1
 			// 
-			this->checkBox1->AutoSize = true;
-			this->checkBox1->Location = System::Drawing::Point(438, 116);
-			this->checkBox1->Name = L"checkBox1";
-			this->checkBox1->Size = System::Drawing::Size(80, 17);
-			this->checkBox1->TabIndex = 3;
-			this->checkBox1->Text = L"checkBox1";
-			this->checkBox1->UseVisualStyleBackColor = true;
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(12, 9);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(211, 18);
+			this->label1->TabIndex = 5;
+			this->label1->Text = L"PARÁMETROS MOVIMIENTO";
+			this->label1->Click += gcnew System::EventHandler(this, &Form1::label1_Click);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(12, 45);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(99, 13);
+			this->label3->TabIndex = 6;
+			this->label3->Text = L"STEPS PER UNIT:";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(142, 72);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(43, 20);
+			this->textBox2->TabIndex = 7;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(241, 72);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(43, 20);
+			this->textBox3->TabIndex = 8;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(138, 111);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(43, 20);
+			this->textBox4->TabIndex = 9;
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(58, 171);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(43, 20);
+			this->textBox5->TabIndex = 10;
+			// 
+			// textBox6
+			// 
+			this->textBox6->Location = System::Drawing::Point(142, 171);
+			this->textBox6->Name = L"textBox6";
+			this->textBox6->Size = System::Drawing::Size(43, 20);
+			this->textBox6->TabIndex = 11;
+			// 
+			// textBox7
+			// 
+			this->textBox7->Location = System::Drawing::Point(241, 171);
+			this->textBox7->Name = L"textBox7";
+			this->textBox7->Size = System::Drawing::Size(43, 20);
+			this->textBox7->TabIndex = 12;
+			// 
+			// textBox8
+			// 
+			this->textBox8->Location = System::Drawing::Point(138, 221);
+			this->textBox8->Name = L"textBox8";
+			this->textBox8->Size = System::Drawing::Size(43, 20);
+			this->textBox8->TabIndex = 13;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(23, 178);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(29, 13);
+			this->label4->TabIndex = 14;
+			this->label4->Text = L"eje x";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(23, 75);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(29, 13);
+			this->label5->TabIndex = 15;
+			this->label5->Text = L"eje x";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(107, 75);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(29, 13);
+			this->label6->TabIndex = 16;
+			this->label6->Text = L"eje x";
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Location = System::Drawing::Point(107, 178);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(29, 13);
+			this->label7->TabIndex = 17;
+			this->label7->Text = L"eje x";
+			// 
+			// label8
+			// 
+			this->label8->AutoSize = true;
+			this->label8->Location = System::Drawing::Point(206, 75);
+			this->label8->Name = L"label8";
+			this->label8->Size = System::Drawing::Size(29, 13);
+			this->label8->TabIndex = 18;
+			this->label8->Text = L"eje x";
+			// 
+			// label9
+			// 
+			this->label9->AutoSize = true;
+			this->label9->Location = System::Drawing::Point(206, 178);
+			this->label9->Name = L"label9";
+			this->label9->Size = System::Drawing::Size(29, 13);
+			this->label9->TabIndex = 19;
+			this->label9->Text = L"eje x";
+			// 
+			// label10
+			// 
+			this->label10->AutoSize = true;
+			this->label10->Location = System::Drawing::Point(103, 228);
+			this->label10->Name = L"label10";
+			this->label10->Size = System::Drawing::Size(29, 13);
+			this->label10->TabIndex = 20;
+			this->label10->Text = L"eje x";
+			// 
+			// trackBar1
+			// 
+			this->trackBar1->Location = System::Drawing::Point(348, 358);
+			this->trackBar1->Name = L"trackBar1";
+			this->trackBar1->Size = System::Drawing::Size(310, 45);
+			this->trackBar1->TabIndex = 21;
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(787, 468);
-			this->Controls->Add(this->checkBox1);
-			this->Controls->Add(this->button1);
-			this->Controls->Add(this->label2);
+			this->ClientSize = System::Drawing::Size(1057, 526);
+			this->Controls->Add(this->trackBar1);
+			this->Controls->Add(this->label10);
+			this->Controls->Add(this->label9);
+			this->Controls->Add(this->label8);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->textBox8);
+			this->Controls->Add(this->textBox7);
+			this->Controls->Add(this->textBox6);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label2);
 			this->Name = L"Form1";
 			this->Text = L"Interfaz para dummies";
 			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBar1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -120,5 +288,7 @@ namespace CppCLRWinFormsProject {
 	}
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
-	};
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
